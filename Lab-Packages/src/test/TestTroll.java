b89d2ee9c88687dd8fa371253c88c58cc28d3979
AHMED <eng.computer1@yahoo.com>
2021-05-09 11:24:10 -0500

    Packages

---
+++ Lab-Packages/src/test/TestTroll.java
@@ -0,0 +1,12 @@
+package test;
+
+import model.Troll;
+import model.trolls.AnotherTroll;
+
+public class TestTroll {
+	public static void main(String[] args) {
+		Troll troll =new Troll();
+		AnotherTroll anothertroll =new AnotherTroll();
+	}
+
+}
