096f8762348c70bd39a32880e8ea3d592a5e1c1e
AHMED <eng.computer1@yahoo.com>
2021-05-16 22:14:03 -0500

    trycatch

---
+++ morningChallange/src/morningChallange/TryCatch.java
@@ -0,0 +1,31 @@
+package morningChallange;
+
+public class TryCatch {
+	
+	
+	public static void main(String[] args) {
+		
+		int[] arr= {2,4,5,6,8};
+		try {
+		   // int k = arr[8];
+		//	System.out.println("k = "+k);
+			System.out.println("in try 1");
+			badmethod();
+			System.out.println("in try 2");
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("in catch");
+		}
+	}
+	
+	public static void badmethod() {
+		// TODO Auto-generated method stub
+		System.out.println("in method");
+		throw new Error();
+
+	}
+	
+
+
+}
